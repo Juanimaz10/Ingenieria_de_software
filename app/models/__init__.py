722b4c0784922ab3772c877da42435a7db6e9a5b
Eliel Mato <elielmato360@gmail.com>
2025-04-14 18:08:14 -0300

    #27 Models Receipt

--- app/models/__init__.py
+++ app/models/__init__.py
@@ -2,3 +2,4 @@ from .article import Article
 from .notification import Notification
 from .brand import Brand
 from .category import Category
+from .receipt import Receipt, ReceiptFooter, ReceiptHeader, ReceiptItem, ReceiptType
