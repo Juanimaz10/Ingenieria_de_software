7957c0662a59e43703db8f9fee4c56415475d805
Pachi69 <pachisepulveda53@gmail.com>
2025-04-21 17:25:48 -0300

    #22 Modificacion test_article

--- test/test_article.py
+++ test/test_article.py
@@ -2,7 +2,7 @@ import os
 import unittest
 from app import create_app
 from app import db
-from app.models import Article
+from app.models import Article, Category, Brand
 from app.services import ArticleService
 
 
@@ -31,8 +31,6 @@ class ArticleTestCase(unittest.TestCase):
         self.assertIsNotNone(article)
         self.assertEqual(article.name, 'Tupu')
         self.assertEqual(article.description, 'description')
-        self.assertEqual(article.category, 'coso')
-        self.assertEqual(article.brand, 'cosito')
         self.assertEqual(article.minimun_stock, 1)
         self.assertEqual(article.code_ean13, 'abc')
 
@@ -54,6 +52,8 @@ class ArticleTestCase(unittest.TestCase):
         article = self.__new_article()
         article2 = self.__new_article()
         article2.id_article = 2
+        article2.category.id = 2
+        article2.brand.id = 2
         article_save = ArticleService.save(article)
         article2_save = ArticleService.save(article2)
         self.check_data(article_save)
@@ -78,18 +78,36 @@ class ArticleTestCase(unittest.TestCase):
         self.assertEqual(article_save_update.description, 'new description')
         self.assertEqual(article_save.description, article_save_update.description)
         self.assertEqual(article.description, article_save_update.description)
+    
+    def __new_category(self):
+        category = Category()
+        category.id = 1
+        category.name = 'Prueba'
+        category.description = 'descripcion de prueba'
 
+        return category
+    
+    def __new_brand(self):
+        brand = Brand()
+        brand.id = 1
+        brand.name = "Marca"
+        brand.description = "Una Marca"
+        return brand
+    
     def __new_article(self):
         article = Article()
+        category = self.__new_category()
+        brand = self.__new_brand()
         article.id_article = 1
         article.name = 'Tupu'
         article.description = 'description'
-        article.category = 'coso'
-        article.brand = 'cosito'
+        article.category = category
+        article.brand = brand
         article.minimun_stock = 1
         article.code_ean13 = 'abc'
         return article
-    
+
+
     def check_data(self, article_save):
         self.assertIsNotNone(article_save)
         self.assertIsNotNone(article_save.id_article)
