db4e4f309fe3721dfc06b28b6fabc941ddd50b0e
Eliel Mato <elielmato360@gmail.com>
2025-04-22 16:47:37 -0300

    #32 Refactorizacion de Marca, Articulo y Categoria

--- test/test_article.py
+++ test/test_article.py
@@ -2,10 +2,11 @@ import os
 import unittest
 from app import create_app
 from app import db
-from app.models import Article, Category, Brand
 from app.services import ArticleService
-
-
+from utils import new_article, new_brand, new_category
+brand = new_brand(name='Marca', description='Una Marca')
+category = new_category(name='Category', description='Una Categoria')
+article = new_article(name='Tupu', description='description', category=category.id, brand=brand.id, minimun_stock=1, code_ean13='abc')
 
 class ArticleTestCase(unittest.TestCase):
     """
@@ -27,7 +28,6 @@ class ArticleTestCase(unittest.TestCase):
         self.app_context.pop()
 
     def test_article(self):
-        article = self.__new_article()
         self.assertIsNotNone(article)
         self.assertEqual(article.name, 'Tupu')
         self.assertEqual(article.description, 'description')
@@ -35,25 +35,17 @@ class ArticleTestCase(unittest.TestCase):
         self.assertEqual(article.code_ean13, 'abc')
 
     def test_save(self):
-        article = self.__new_article()
         article_save = ArticleService.save(article)
         self.check_data(article_save)
-        article_delete = ArticleService.delete(article)
-        self.assertIsNone(article_delete)
 
     def test_find(self):
-        article = self.__new_article()
         article_save = ArticleService.save(article)
         self.check_data(article_save)
-        article = ArticleService.find(1)
-        self.check_data(article_save)
+        article_find = ArticleService.find(article_save.id_article)
+        self.check_data(article_find)
 
     def test_find_all(self):
-        article = self.__new_article()
-        article2 = self.__new_article()
-        article2.id_article = 2
-        article2.category.id = 2
-        article2.brand.id = 2
+        article2 = new_article(name='Tupu2', description='description2', category=category.id, brand=brand.id, minimun_stock=1, code_ean13='abc')
         article_save = ArticleService.save(article)
         article2_save = ArticleService.save(article2)
         self.check_data(article_save)
@@ -63,50 +55,19 @@ class ArticleTestCase(unittest.TestCase):
         self.assertGreater(len(articles), 1)
 
     def test_find_by(self):
-        article = self.__new_article()
         article_save = ArticleService.save(article)
         self.check_data(article_save)
-        article = ArticleService.find_by(description = 'description')
-        self.assertIsNotNone(article)
-        self.assertGreater(len(article), 0)
+        article_save = ArticleService.find_by(description = 'description')
+        self.assertIsNotNone(article_save)
+        self.assertGreater(len(article_save), 0)
 
     def test_update(self):
-        article = self.__new_article()
         article_save = ArticleService.save(article)
         article_save.description = 'new description'
         article_save_update = ArticleService.update(article_save)
         self.assertEqual(article_save_update.description, 'new description')
         self.assertEqual(article_save.description, article_save_update.description)
         self.assertEqual(article.description, article_save_update.description)
-    
-    def __new_category(self):
-        category = Category()
-        category.id = 1
-        category.name = 'Prueba'
-        category.description = 'descripcion de prueba'
-
-        return category
-    
-    def __new_brand(self):
-        brand = Brand()
-        brand.id = 1
-        brand.name = "Marca"
-        brand.description = "Una Marca"
-        return brand
-    
-    def __new_article(self):
-        article = Article()
-        category = self.__new_category()
-        brand = self.__new_brand()
-        article.id_article = 1
-        article.name = 'Tupu'
-        article.description = 'description'
-        article.category = category
-        article.brand = brand
-        article.minimun_stock = 1
-        article.code_ean13 = 'abc'
-        return article
-
 
     def check_data(self, article_save):
         self.assertIsNotNone(article_save)
---
+++ test/utils/__init__.py
@@ -0,0 +1,3 @@
+from .article_utils import new_article
+from .category_utils import new_category
+from .brand_utils import new_brand
