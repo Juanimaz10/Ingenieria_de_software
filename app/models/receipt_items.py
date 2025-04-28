bcb2e0e2c44079e39cadbdbce536c3b479220047
Pachi69 <pachisepulveda53@gmail.com>
2025-04-28 17:22:56 -0300

    #22 Arreglado errores de modelos

--- app/models/receipt_items.py
+++ app/models/receipt_items.py
@@ -1,9 +1,5 @@
 from dataclasses import dataclass
 from app import db
-from app.models import Article, Receipt
-from sqlalchemy.orm import Mapped
-
-
 
 
 @dataclass(init=True, eq=True)
@@ -14,8 +10,10 @@ class ReceiptItem(db.Model):
     __tablename__ = 'receipt_items'
     id: int = db.Column('id', db.Integer, primary_key=True, autoincrement=True)
     id_article: int = db.Column('id_article', db.Integer, db.ForeignKey('articles.id'), nullable=False)
-    article: Mapped["Article"] = db.relationship('Article', back_populates='receipt_items', lazy=True)
     quantity: int = db.Column('quantity', db.Float, nullable=False, default=0)
     batch_id: int = db.Column('batch', db.Integer[100], nullable=False)
-    receipt = db.relationship('Receipt', back_populates='items', lazy=True) 
     receipt_id: int = db.Column('id_receipt', db.Integer, db.ForeignKey('receipts.id'), nullable=False)
+
+
+    receipt = db.relationship('Receipt', back_populates='items', lazy=True) 
+    article = db.relationship('Article', back_populates='receipt_items', lazy=True)
--- app/services/article_services.py
+++ app/services/article_services.py
@@ -5,23 +5,28 @@ from typing import List
 
 class ArticleService():
     
+    @staticmethod
     def save(article: Article) -> 'Article':
-
         ArticleRepository.save(article)
         return article
     
+    @staticmethod
     def delete(article: 'Article') -> None:
         ArticleRepository.delete(article)
-
+    
+    @staticmethod
     def find(id: int) -> 'Article':
         return ArticleRepository.find(id)
     
+    @staticmethod
     def find_all() -> List['Article']:
         return ArticleRepository.find_all()
     
+    @staticmethod
     def find_by(**kwargs) -> List['Article']:
         return ArticleRepository.find_by(**kwargs)
     
+    @staticmethod
     def update(article: 'Article') -> 'Article':
         ArticleRepository.update(article)
         return article
