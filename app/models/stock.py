b6f91790e9a27402817f077e8f7da727c6d50ad1
juanimaz10 <j.imaz@alumno.um.edu.ar>
2025-04-28 17:58:10 -0300

    #31 modificación de stock por @staticMethod en repostiories

--- app/models/stock.py
+++ app/models/stock.py
@@ -1,7 +1,6 @@
 from dataclasses import dataclass
 from app import db
 
-
 @dataclass(init=True, eq=True)
 class Stock(db.Model):
     __tablename__ = 'stock'
@@ -9,8 +8,6 @@ class Stock(db.Model):
     article_id: int = db.Column('id_article', db.Integer, db.ForeignKey('articles.id'), nullable=False)
     quantity = db.Column(db.Integer, nullable=False, default=0)
     batch_id: int = db.Column('batch_id', db.Integer, db.ForeignKey('batches.id'), nullable=False)
-
-
     article = db.relationship('Article', back_populates='receipt_items', lazy=True)
     batch = db.relationship('Batch', lazy=True)
 
