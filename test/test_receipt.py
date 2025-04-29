bda00145ce0ff009f6fbb2c27824b7c78d2ee05d
juanimaz10 <j.imaz@alumno.um.edu.ar>
2025-04-29 19:29:38 -0300

    Refactor receipt utility functions and tests: update new_receipt function signature to accept ReceiptType, enhance test cases for receipt creation and saving.

--- test/test_receipt.py
+++ test/test_receipt.py
@@ -2,6 +2,9 @@ from os import name
 import os
 import unittest
 from app import db, create_app
+from utils import new_receipt, new_receipt_type, new_brand, new_article, new_batch, new_receipt_items
+from app.services import ReceiptService
+from datetime import datetime
 
 class TestReceiptService(unittest.TestCase):
     def setUp(self):
@@ -15,6 +18,25 @@ class TestReceiptService(unittest.TestCase):
         db.session.remove()
         db.drop_all()
         self.app_context.pop()
+    
+    def test_receipt(self):
+        receipt_type = new_receipt_type(name = "remito de entrada", description = "remito de entrada", type_entry = 1)
+        receipt = new_receipt(id_footer=1, id_header=1, receipt_type=receipt_type)
+        self.assertIsNotNone(receipt)
+
+    def test_save_receipt(self):
+        brand = new_brand(name = "brand", description = "brand")
+        article = new_article(name = "name", description = "article", brand = brand, category = new_category, minimun_stock = 10, code_ean13 = "1234567890123")
+        batch = new_batch(article = article, quantity = 10, expiration_date = datetime.now(), items = 10)
+        receipt_items = new_receipt_items(article = article, batch = batch, quantity = 10, receipt = receipt)
+        receipt_type = new_receipt_type(name = "remito de entrada", description = "remito de entrada", type_entry = 1)
+        receipt = new_receipt(id_footer=1, id_header=1, receipt_type=receipt_type)
+        receipt.items.append(receipt_items)
+        ReceiptService.save(receipt)
+        self.assertIsNotNone(receipt)
+    
+
+        
 
     
 if __name__ == '__main__':
--- test/utils/receipt_utils.py
+++ test/utils/receipt_utils.py
@@ -1,8 +1,10 @@
-from app.models import Receipt
+from app.models import Receipt, ReceiptType
 
-def new_receipt(id_receipt_type: int, id_header: int, id_footer: int) -> Receipt:
+def new_receipt(receipt_type: ReceiptType, id_header: int, id_footer: int) -> Receipt:
     receipt = Receipt()
     receipt.id_header = id_header
     receipt.id_footer = id_footer
-    receipt.id_receipt_type = id_receipt_type
+    receipt.receipt_type = receipt_type
+    
+
     return receipt
