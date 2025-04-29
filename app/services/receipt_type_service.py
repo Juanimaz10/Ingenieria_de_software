85f94d189119d0812c03cb226bd1b8c1bf3948ba
juanimaz10 <j.imaz@alumno.um.edu.ar>
2025-04-29 17:28:21 -0300

    Refactor ReceiptTypesService and StockService: replace instance of StockRepository with direct calls to repository methods for improved clarity and consistency.

--- app/services/receipt_type_service.py
+++ app/services/receipt_type_service.py
@@ -5,8 +5,8 @@ from app.repositories import ReceiptTypeRepository
 class ReceiptTypesService:
 
     @staticmethod
-    def find(id: int) -> 'ReceiptTypeRepository':
-        receipt_type = ReceiptType.find(id)
+    def find(id: int) -> ReceiptType:
+        receipt_type = ReceiptTypeRepository.find(id)
         if not receipt_type:
                 raise ValueError(f"Receipt type with ID {id} not found.")
         return receipt_type
